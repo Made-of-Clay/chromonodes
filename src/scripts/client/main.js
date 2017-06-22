28d383e6b1029bb53ac22fde457809d77a265dc4
Made-of-Clay <adam.m.leis@gmail.com>
2017-06-22 08:11:26 -0400

    add todos to main.js

--- src/scripts/client/main.js
+++ src/scripts/client/main.js
@@ -7,13 +7,13 @@ import state from './state.js';
 animate();
 
 /*
-- setup pixi canvas
-- create user on page load
+TODO: setup pixi canvas
+TODO: create user on page load
 -- assign color and guid to user
 -- push user to state object
-- get touch-add working
+TODO: get touch-add working
 -- create node using user's color
 -- save node info to nodes array
-- share state over websockets
-- update state object when socket info comes in
+TODO: share state over websockets
+TODO: update state object when socket info comes in
  */
